ce137e41da97a019d5f5a3c66b3b5af1bf873bd5
Jared <TheWolfBadger@gmail.com>
2020-06-24 11:39:43 -0400

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,28 +2,16 @@ fx_version 'adamant'
 
 game 'gta5'
 
-description 'ESX Door Lock'
+description 'Badger Door Lock'
 
 version '1.4.0'
 
 server_scripts {
-	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
-	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
 	'config.lua',
 	'client/main.lua'
 }
-
-dependency 'es_extended'
