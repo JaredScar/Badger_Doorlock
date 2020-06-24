fad1c052b1410cf731fc273dc91eaa30dfab0b58
Jared <TheWolfBadger@gmail.com>
2020-06-23 22:12:11 -0400

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,6 +1,13 @@
 Config = {}
 Config.Locale = 'en'
 
+Config.Roles = {
+	['SAHP'] = 720070416844193854,
+	['BCSO'] = 720071486416617575,
+	['BCPD'] = 720078261794897941,
+	['CO'] = 720311990941122580,
+}
+
 Config.DoorList = {
 
 	--
@@ -10,7 +17,7 @@ Config.DoorList = {
 	-- Entrance Doors
 	{
 		textCoords = vector3(434.7, -982.0, 31.5),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = false,
 		maxDistance = 2.5,
 		doors = {
@@ -25,7 +32,7 @@ Config.DoorList = {
 		objHeading = 90.0,
 		objCoords = vector3(449.6, -986.4, 30.6),
 		textCoords = vector3(450.1, -986.3, 31.7),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 1.25
 	},
@@ -36,7 +43,7 @@ Config.DoorList = {
 		objHeading = 90.0,
 		objCoords = vector3(464.3, -984.6, 43.8),
 		textCoords = vector3(464.3, -984.0, 44.8),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 1.25
 	},
@@ -47,7 +54,7 @@ Config.DoorList = {
 		objHeading = 90.0,
 		objCoords = vector3(461.2, -985.3, 30.8),
 		textCoords = vector3(461.5, -986.0, 31.5),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 1.25
 	},
@@ -58,7 +65,7 @@ Config.DoorList = {
 		objHeading = 270.0,
 		objCoords = vector3(452.6, -982.7, 30.6),
 		textCoords = vector3(453.0, -982.6, 31.7),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 1.25
 	},
@@ -69,7 +76,7 @@ Config.DoorList = {
 		objHeading = 180.0,
 		objCoords = vector3(447.2, -980.6, 30.6),
 		textCoords = vector3(447.2, -980.0, 31.7),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 1.25
 	},
@@ -77,7 +84,7 @@ Config.DoorList = {
 	-- To downstairs (double doors)
 	{
 		textCoords = vector3(444.6, -989.4, 31.7),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 4,
 		doors = {
@@ -96,7 +103,7 @@ Config.DoorList = {
 		objHeading = 0.0,
 		objCoords = vector3(463.8, -992.6, 24.9),
 		textCoords = vector3(463.3, -992.6, 25.1),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 1.25
 	},
@@ -107,7 +114,7 @@ Config.DoorList = {
 		objHeading = 270.0,
 		objCoords = vector3(462.3, -993.6, 24.9),
 		textCoords = vector3(461.8, -993.3, 25.0),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 1.25
 	},
@@ -118,7 +125,7 @@ Config.DoorList = {
 		objHeading = 90.0,
 		objCoords = vector3(462.3, -998.1, 24.9),
 		textCoords = vector3(461.8, -998.8, 25.0),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 1.25
 	},
@@ -129,7 +136,7 @@ Config.DoorList = {
 		objHeading = 90.0,
 		objCoords = vector3(462.7, -1001.9, 24.9),
 		textCoords = vector3(461.8, -1002.4, 25.0),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 1.25
 	},
@@ -140,7 +147,7 @@ Config.DoorList = {
 		objHeading = 0.0,
 		objCoords = vector3(463.4, -1003.5, 25.0),
 		textCoords = vector3(464.0, -1003.5, 25.5),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 1.25
 	},
@@ -152,7 +159,7 @@ Config.DoorList = {
 	-- Back (double doors)
 	{
 		textCoords = vector3(468.6, -1014.4, 27.1),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 4,
 		doors = {
@@ -167,7 +174,7 @@ Config.DoorList = {
 		objHeading = 90.0,
 		objCoords = vector3(488.8, -1017.2, 27.1),
 		textCoords = vector3(488.8, -1020.2, 30.0),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 14,
 		size = 2
@@ -183,7 +190,7 @@ Config.DoorList = {
 		objHeading = 30.0,
 		objCoords = vector3(1855.1, 3683.5, 34.2),
 		textCoords = vector3(1855.1, 3683.5, 35.0),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = false,
 		maxDistance = 1.25
 	},
@@ -195,7 +202,7 @@ Config.DoorList = {
 	-- Entrance (double doors)
 	{
 		textCoords = vector3(-443.5, 6016.3, 32.0),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = false,
 		maxDistance = 2.5,
 		doors = {
@@ -213,7 +220,7 @@ Config.DoorList = {
 		objHash = GetHashKey('prop_gate_prison_01'),
 		objCoords = vector3(1844.9, 2604.8, 44.6),
 		textCoords = vector3(1844.9, 2608.5, 48.0),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 12,
 		size = 2
@@ -223,7 +230,7 @@ Config.DoorList = {
 		objHash = GetHashKey('prop_gate_prison_01'),
 		objCoords = vector3(1818.5, 2604.8, 44.6),
 		textCoords = vector3(1818.5, 2608.4, 48.0),
-		authorizedJobs = {'police'},
+		authorizedRoles = {'CO', 'SAHP', 'BCSO', 'BCPD'},
 		locked = true,
 		maxDistance = 12,
 		size = 2
